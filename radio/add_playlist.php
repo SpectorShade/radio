1a46a413e16c4ed5e2cde5f4d4eba77bbc2ba8b6
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-02-25 21:38:05 -0700

    Update add_playlist.php

--- radio/add_playlist.php
+++ radio/add_playlist.php
@@ -1,9 +1,13 @@
 <?php
 include 'config.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['programaID'], $_POST['cancionID'], $_POST['posicion'])) {
+    $programaID = (int)$_POST['programaID'];
+    $cancionID = (int)$_POST['cancionID'];
+    $posicion = (int)$_POST['posicion'];
+
     $stmt = $conexion->prepare("INSERT INTO playlist (ProgramaID, CancionID, Posicion) VALUES (?, ?, ?)");
-    $stmt->bind_param("iii", $_POST['programaID'], $_POST['cancionID'], $_POST['posicion']);
+    $stmt->bind_param("iii", $programaID, $cancionID, $posicion);
 
     echo $stmt->execute()
         ? "<script>alert('Playlist entry added successfully!'); window.location.href='index.php';</script>"
