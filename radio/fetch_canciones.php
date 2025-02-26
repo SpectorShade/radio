0699e774f14d25fbe91d7f23f25e9246676e0584
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-02-25 22:29:44 -0700

    Delete radio directory

--- radio/fetch_canciones.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-
-$result = $conexion->query("SELECT ID, Titulo FROM cancion ORDER BY Titulo");
-
-while ($row = $result->fetch_assoc()) {
-    echo "<option value='{$row['ID']}'>" . htmlspecialchars($row['Titulo']) . "</option>";
-}
-?>
--- radio/fetch_generos.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'config.php';
-
-$result = $conexion->query("SELECT ID, Nombre FROM genero ORDER BY Nombre");
-
-while ($row = $result->fetch_assoc()) {
-    echo "<option value='{$row['ID']}'>" . htmlspecialchars($row['Nombre']) . "</option>";
-}
-?>
