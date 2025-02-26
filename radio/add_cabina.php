e2e75f33d461868fa9a28f154fad460eabf0d1b6
SpectorShade <92053721+SpectorShade@users.noreply.github.com>
2025-02-25 21:35:13 -0700

    Update add_cabina.php

--- radio/add_cabina.php
+++ radio/add_cabina.php
@@ -1,9 +1,13 @@
 <?php
 include 'config.php';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nombre'], $_POST['locacion'], $_POST['capacidad'])) {
+    $nombre = $conexion->real_escape_string($_POST['nombre']);
+    $locacion = $conexion->real_escape_string($_POST['locacion']);
+    $capacidad = (int)$_POST['capacidad'];
+
     $stmt = $conexion->prepare("INSERT INTO cabina (Nombre, Locacion, Capacidad) VALUES (?, ?, ?)");
-    $stmt->bind_param("ssi", $_POST['nombre'], $_POST['locacion'], $_POST['capacidad']);
+    $stmt->bind_param("ssi", $nombre, $locacion, $capacidad);
 
     echo $stmt->execute()
         ? "<script>alert('Booth added successfully!'); window.location.href='index.php';</script>"
